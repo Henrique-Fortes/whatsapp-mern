e4c640abb51134ee9c5b36d8cce59bc2bcc39e4d
Henrique H. da Silva Fortes <Henrique Fortes>
2021-01-08 16:42:05 -0300

    commit whatsapp-mern-front-end

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6KLpq_TQ2nZwx03Je6trhdeghOVUeRb8",
+    authDomain: "whatsapp-mern-2bda9.firebaseapp.com",
+    projectId: "whatsapp-mern-2bda9",
+    storageBucket: "whatsapp-mern-2bda9.appspot.com",
+    messagingSenderId: "692948974217",
+    appId: "1:692948974217:web:8246c5f49d7e0700f2e458"
+  };
+  
